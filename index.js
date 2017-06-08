fe2f953877acf6c73df1dc9753893b4bc8c1fe1a
Mark Kemper <mark@hypothesisconsulting.com.au>
2017-06-08 14:38:14 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    response.end('Hello from Azure running node version: ' + process.version + '');
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
